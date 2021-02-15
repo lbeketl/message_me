fbe01ec1960763881e550540cbf36751054b2f0f
beket <lbeketl@mail.com>
2021-02-15 22:35:26 +0200

    Add icon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
